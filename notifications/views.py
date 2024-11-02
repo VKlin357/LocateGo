a59cd92dc03ddb61fab3eca151a537b0b1338cb6
VKlin357 <142524793+VKlin357@users.noreply.github.com>
2024-11-02 06:10:43 +0300

    Create views.py

---
+++ notifications/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from .models import Notification
+from .serializers import NotificationSerializer
+
+class NotificationListView(generics.ListAPIView):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
