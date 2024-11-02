729083602c0e8e05c8aacf8c22c4b62609402211
VKlin357 <142524793+VKlin357@users.noreply.github.com>
2024-11-02 06:00:44 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,42 @@
+import os
+
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'channels',
+    'users',
+    'applications',
+    'chat',
+    'notifications',
+]
+
+ASGI_APPLICATION = 'locatego.asgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
